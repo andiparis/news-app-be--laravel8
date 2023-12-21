a6133ec8e5be8017653a54a1d58d8e7a68ac4071
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-12-21 12:39:45 +0700

    Add relation for tables users, categories, & posts

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,19 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Category extends Model
 {
   use HasFactory;
 
   protected $guarded = ['id'];
 
-  public function category()
+  public function Post()
   {
-    return $this->belongsTo(Category::class);
-  }
-
-  public function user()
-  {
-    return $this->belongsTo(User::class);
+    return $this->hasMany(Post::class);
   }
 }

3b37dac136c90889d75319b7141fa337c9fc657f
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-12-26 20:43:19 +0700

    Add link to author page

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
 
 <h2>{{ $post->title }}</h2>
 <p>
-  By <a href="#">{{ $post->user->name }}</a>
+  By <a href="/author/{{ $post->author->username }}">{{ $post->author->name }}</a>
   in <a href="/category/{{ $post->category->slug }}">{{ $post->category->name }}</a>
 </p>
 {!! $post->body !!}
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,7 +10,7 @@
     <a href="/post/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
   </h2>
   <p>
-    By <a href="#">{{ $post->user->name }}</a>
+    By <a href="/author/{{ $post->author->username }}">{{ $post->author->name }}</a>
     in <a href="/category/{{ $post->category->slug }}">{{ $post->category->name }}</a>
   </p>
   <p>{{ $post->excerpt }}</p>

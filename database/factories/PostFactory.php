1491b8781140b3a78a29299d770e159fe4f0fd1f
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-12-27 12:52:31 +0700

    Update post factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,7 +17,9 @@ class PostFactory extends Factory
       'title'       => $this->faker->sentence(mt_rand(5, 10)),
       'slug'        => $this->faker->slug(mt_rand(5, 10)),
       'excerpt'     => $this->faker->paragraph(1),
-      'body'        => $this->faker->paragraph(mt_rand(4, 8)),
+      'body'        => collect($this->faker->paragraphs(mt_rand(4, 8)))
+        ->map(fn ($paragraph) => "<p>$paragraph</p>")
+        ->implode(''),
       'category_id' => mt_rand(1, 3),
       'user_id'     => mt_rand(1, 5),
     ];

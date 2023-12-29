14498bdbe4f0fba04647d76fcc3669e4bb64bca7
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-12-29 16:58:14 +0700

    Add search in posts using keyword, category, & author as a parameter

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,17 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
   public function index()
   {
+    $title = '';
+
+    if (request('category')) {
+      $category = Category::firstWhere('slug', request('category'));
+      $title = ' in ' . $category->name;
+    }
+    if (request('author')) {
+      $author = User::firstWhere('username', request('author'));
+      $title = ' in ' . $author->name;
+    }
+
     return view('posts', [
-      'title'     => 'All Posts',
+      'title'     => 'All Posts' . $title,
       'active'    => 'posts',
-      'posts'     => Post::latest()->get(),
+      'posts'     => Post::latest()->filter(request(['search', 'category', 'author']))->get(),
     ]);
   }
 
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -21,4 +21,26 @@ class Post extends Model
   {
     return $this->belongsTo(User::class, 'user_id');
   }
+
+  public function scopeFilter($query, array $filters)
+  {
+    $query->when($filters['search'] ?? false, function ($query, $search) {
+      return $query->where('title', 'like', '%' . $search . '%')
+        ->orWhere('body', 'like', '%' . $search . '%');
+    });
+
+    $query->when($filters['category'] ?? false, function ($query, $category) {
+      return $query
+        ->whereHas('category', function ($query) use ($category) {
+          $query->where('slug', $category);
+        });
+    });
+
+    $query->when(
+      $filters['author'] ?? false,
+      fn ($query, $author) =>
+      $query->whereHas('author', fn ($query) =>
+      $query->where('username', $author))
+    );
+  }
 }

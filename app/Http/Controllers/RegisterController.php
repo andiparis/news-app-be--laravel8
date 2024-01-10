8e27549041ee43ea367a0bcb6f36de26b3340613
Andi Paris Bachtiar <andiparis02@gmail.com>
2024-01-10 11:42:51 +0700

    Add method for registering user

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,5 +14,20 @@ class RegisterController extends Controller
       'title'   => 'Register',
       'active'  => 'register',
     ]);
+  }
+
+  public function store(Request $request)
+  {
+    $validatedData = $request->validate([
+      'name'        => 'required|max:255',
+      'username'    => 'required|min:4|max:255|unique:users',
+      'email'       => 'required|email|max:255|unique:users',
+      'password'    => 'required|min:8|max:255',
+    ]);
+    $validatedData['password'] = Hash::make($validatedData['password']);
+
+    User::create($validatedData);
+
+    return redirect('/login')->with('success', 'Registration successfull! Please login');
   }
 }

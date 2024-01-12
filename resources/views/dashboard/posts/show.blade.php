a34496721344e0bd57a9aaf0ea8f1ca5d8da968b
Andi Paris Bachtiar <andiparis02@gmail.com>
2024-01-12 14:09:21 +0700

    Add detail for each post

---
+++ resources/views/dashboard/posts/show.blade.php
@@ -0,0 +1,21 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+
+<div class="container">
+  <div class="row my-3">
+    <div class="col-lg-8">
+      <h2 class="mb-3">{{ $post->title }}</h2>
+      <a href="/dashboard/posts" class="btn btn-success">Back to my posts</a>
+      <a href="" class="btn btn-warning">Edit</a>
+      <a href="" class="btn btn-danger">Delete</a>
+      <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid mt-3">
+
+      <article class="my-3 fs-5">
+        {!! $post->body !!}
+      </article>
+    </div>
+  </div>
+</div>
+
+@endsection

740d00971e78aa81e9a6f74db5eca56bd9c7309b
Andi Paris Bachtiar <andiparis02@gmail.com>
2024-01-12 14:06:47 +0700

    Add class active based on url

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -7,7 +7,7 @@
     <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
       <ul class="nav flex-column">
         <li class="nav-item">
-          <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="/dashboard">
+          <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dasboard') ? 'active' : '' }}" href="/dashboard">
             <svg class="bi">
               <use xlink:href="#house-fill" />
             </svg>
@@ -15,7 +15,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link d-flex align-items-center gap-2" href="/dashboard/posts">
+          <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard/posts') ? 'active' : '' }}" href="/dashboard/posts">
             <svg class="bi">
               <use xlink:href="#file-earmark" />
             </svg>

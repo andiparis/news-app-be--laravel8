5e23ab9136f6eaebfc5c1db1850bc680f77a00b4
Andi Paris Bachtiar <andiparis02@gmail.com>
2024-01-12 14:08:56 +0700

    Show post data in a table

---
+++ resources/views/dashboard/posts/index.blade.php
@@ -0,0 +1,38 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+  <h1 class="h2">My Posts</h1>
+</div>
+
+<div class="table-responsive">
+  <table class="table table-striped table-sm">
+    <thead>
+      <tr>
+        <td scope="col">#</td>
+        <td scope="col">Title</td>
+        <td scope="col">Category</td>
+        <td scope="col">Action</td>
+      </tr>
+    </thead>
+    <tbody>
+
+      @foreach($posts as $post)
+      <tr>
+        <td>{{ $loop->iteration }}</td>
+        <td>{{ $post->title }}</td>
+        <td>{{ $post->category->name }}</td>
+        <td>
+          <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info">Show</a>
+          <a href="" class="badge bg-danger">Edit</a>
+          <a href="" class="badge bg-warning">Delete</a>
+        </td>
+      </tr>
+      @endforeach
+
+    </tbody>
+  </table>
+</div>
+
+@endsection

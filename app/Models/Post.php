82963cc532d9715d435f562b5f2eebde779fc957
Andi Paris Bachtiar <andiparis02@gmail.com>
2024-02-15 11:45:36 +0700

    Add sluggable method

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-  use HasFactory;
+  use HasFactory, Sluggable;
 
   protected $guarded = ['id'];
   protected $with = ['category', 'author'];
@@ -47,5 +48,14 @@ class Post extends Model
       $query->whereHas('author', fn ($query) =>
       $query->where('username', $author))
     );
+  }
+
+  public function sluggable(): array
+  {
+    return [
+      'slug' => [
+        'source' => 'title'
+      ]
+    ];
   }
 }

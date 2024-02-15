525efbc9efe5393c925e44f31c0dc4e4451d5ad1
Andi Paris Bachtiar <andiparis02@gmail.com>
2024-02-15 07:50:38 +0700

    Fixing error when url login accessed

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -49,7 +49,7 @@
         </li>
         @else
         <li class="nav-item">
-          <a href="/login" class="nav-link {{ ($active === 'login') ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+          <a href="/login" class="nav-link {{ Request::is('login') ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
         </li>
         @endauth
       </ul>

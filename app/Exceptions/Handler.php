c51f98fefbf35bcd41a1d3ec30a7f013cbf1f906
Andi Paris Bachtiar <andiparis02@gmail.com>
2024-03-17 14:06:13 +0700

    Change default exception handler to json response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,39 +3,52 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array<int, class-string<Throwable>>
-     */
-    protected $dontReport = [
-        //
-    ];
+  /**
+   * A list of the exception types that are not reported.
+   *
+   * @var array<int, class-string<Throwable>>
+   */
+  protected $dontReport = [
+    //
+  ];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array<int, string>
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
+  /**
+   * A list of the inputs that are never flashed for validation exceptions.
+   *
+   * @var array<int, string>
+   */
+  protected $dontFlash = [
+    'current_password',
+    'password',
+    'password_confirmation',
+  ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+  /**
+   * Register the exception handling callbacks for the application.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    $this->reportable(function (Throwable $e) {
+      //
+    });
+  }
+
+  public function render($request, Throwable $exception)
+  {
+    if ($exception instanceof NotFoundHttpException) {
+      return response()->json([
+        'status'  => 'fail',
+        'error'   => 'Your request is not found',
+      ], 404);
     }
+
+    return parent::render($request, $exception);
+  }
 }
